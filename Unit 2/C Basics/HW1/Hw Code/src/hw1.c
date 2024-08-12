5fd978f13e6038635c7d4344da9cbb2a66e08b13
Youssef Khaled <youssef.khaled12382@gmail.com>
2024-08-13 02:22:31 +0300

    C Basics HW

---
+++ Unit 2/C Basics/HW1/Hw Code/src/hw1.c
@@ -0,0 +1,87 @@
+/*
+ ============================================================================
+ Name        : hw1.c
+ Author      : Youssef Khaled
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+//---------------------Q1--------------------
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//	printf("C Programming \n");
+//}
+//-------------------------Q2-------------------
+//#include<stdio.h>
+//int main()
+//{
+//	int x;
+//	printf("Enter an integer: ");
+//	fflush(stdout----);
+//	scanf("%d", &x);
+//	printf("You entered: %d", x);
+//}
+//-------------------------Q3---------------
+//#include<stdio.h>
+//int main()
+//{
+//	int x,y;
+//	printf("Enter two integers : ");
+//	fflush(stdout);
+//	scanf("%d %d", &x,&y);
+//	printf("The sum = %d",(x+y));
+//}
+//-------------------------Q4---------------
+//#include<stdio.h>
+//int main()
+//{
+//	float x,y;
+//	printf("Enter two integers : ");
+//	fflush(stdout);
+//	scanf("%f %f", &x,&y);
+//	printf("Product = %f",(x*y));
+//}
+//-------------------------Q5---------------
+//#include<stdio.h>
+//int main()
+//{
+//    int x;
+//    char c;
+//	printf("Enter a character : ");
+//	fflush(stdout);
+//	scanf("%c", &c);
+//	x=c;
+//	printf("The ascii value of %c = %d",c,x);
+//}
+//-------------------------Q6---------------
+//#include<stdio.h>
+//int main()
+//{
+//    int x,y,temp;
+//    printf("Enter value of x : ");
+//    fflush(stdout);
+//    scanf("%d",&x);
+//    printf("Enter value of y : ");
+//    // fflush(stdout);
+//    scanf("%d",&y);
+//    temp=y;
+//    y=x;
+//    x=temp;
+//    printf("The value of x after swap = %d \n",x);
+//    printf("The value of y after swap = %d \n",y);
+//}
+//-------------------------Q7---------------
+//#include <stdio.h>
+//int main() {
+//    int a, b;
+//    printf("Enter two integers: ");
+//    scanf("%d %d", &a, &b);
+//    printf("Before swapping: a = %d, b = %d\n", a, b);
+//    a = a ^ b;
+//    b = a ^ b;
+//    a = a ^ b;
+//    printf("After swapping: a = %d, b = %d\n", a, b);
+//}
